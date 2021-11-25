638d6bf47d1e82cab49b95cffd14178aa4ba5535
Heisenberg <92525151+HeisenbergChueng@users.noreply.github.com>
2021-11-26 00:40:19 +0800

    Add files via upload

---
+++ python/class 2:#.py
@@ -0,0 +1,13 @@
+print('在一段代碼前面加上井號是註解的意思，代表這行代碼不會運行')
+print()
+print('1.這樣代表可以在python在建工程上做註解，以免日後在幾千行代碼的大型工程中，忘記了這一行代碼是用來幹什麼的')
+print()
+print('例如')
+# 以下代碼用來演示hello world（其實我都唔知點解一定要打hello world
+print('hello would')
+print()
+print()
+print('2.註解行亦可以用來篩錯與分別打兩行代碼試試那行是正確的，在此無法表達自行理解')
+print()
+print()
+print('3.當光標在某一行的時候，你只要按ctrl或command+/，則可以把這一行註解，再按一下則解除')
