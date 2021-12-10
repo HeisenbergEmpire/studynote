877152641fc4bf131341a6100aaa9ed8a8626170
HeisenbergEmpire <92525151+HeisenbergEmpire@users.noreply.github.com>
2021-12-11 00:45:03 +0800

    Add files via upload

---
+++ python/class 7-1:error handling.py
@@ -0,0 +1,16 @@
+# 語法錯誤
+# x = 42
+# y = 206
+# if x == y
+#     print('success')
+print()
+# 運行時錯誤
+# x = 42
+# y = 0
+# print(x/y)
+print()
+# 邏輯錯誤，這種錯誤python不會給你提示的，只能靠自己分析
+x = 206
+y = 42
+if x<y：
+    print(str(x) + 'is greater than' + str(y))
